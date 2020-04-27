a76088c6ad0a2daccce64d9250cb7ecce12bc614
Yi Wang <yi.wang.2005@gmail.com>
2020-04-27 12:58:39 -0700

    Separate graphviz.py out from pandoc_all.bash

--- pandoc_all.bash
+++ pandoc_all.bash
@@ -17,47 +17,13 @@ fi
 echo "Converting Markdown files in $workspace ..."
 cd "$workspace"
 
-cmd='pandoc -s -f markdown+smart --mathjax
-         --highlight-style pygments
-         --columns=200
-         --filter ./.graphviz.py '
-
-css=$(du -a . | grep '\.css$' | cut -f 2 | sort | head -n 1)
-if [[ -f $css ]]; then
-    echo "Using CSS file $css ..."
-    cmd="$cmd --css $css"
-fi
-
-cat <<EOF > .graphviz.py
-#!/usr/bin/env python
-import os
-import sys
-import pygraphviz
-from pandocfilters import toJSONFilter, Para, Image, get_filename4code, get_caption, get_extension, get_value
-
-def graphviz(key, value, format, _):
-    if key == 'CodeBlock':
-        [[ident, classes, keyvals], code] = value
-        if "dot" in classes:
-            caption, typef, keyvals = get_caption(keyvals)
-            prog, keyvals = get_value(keyvals, u"prog", u"dot")
-            filetype = get_extension(format, "png", html="png", latex="pdf")
-            dest = get_filename4code("graphviz", code, filetype)
-            if not os.path.isfile(dest):
-                g = pygraphviz.AGraph(string=code)
-                g.layout()
-                g.draw(dest, prog=prog)
-                sys.stderr.write('Created image ' + dest + '\n')
-            return Para([Image([ident, [], keyvals], caption, [dest, typef])])
-
-if __name__ == "__main__":
-    toJSONFilter(graphviz)
-EOF
-
-chmod +x .graphviz.py
+cmd='pandoc -s -f markdown+smart
+         --pdf-engine xelatex
+         --filter /graphviz.py 
+         --css /nrstyle.css '
 
 for i in $(du -a . | grep '\.md$' | cut -f 2); do
-    target=$(echo $i | sed 's/md$/html/')
+    target=$(echo $i | sed 's/md$/pdf/')
     echo $cmd $i -o $target # && echo "Done: $i -> $target" || echo "Failed to convert: $i"
     $cmd $i -o $target # && echo "Done: $i -> $target" || echo "Failed to convert: $i"
 done
